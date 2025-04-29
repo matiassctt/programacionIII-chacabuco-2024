c788d8a53b6d26aff7b03f086a5a1bb4b31eee1b
Matias Schettino <59831088+matiassctt@users.noreply.github.com>
2025-04-28 21:28:51 -0300

    Add files via upload

---
+++ app/Router/Routes.php
@@ -0,0 +1,19 @@
+<?php 
+
+include_once "Route.php";
+include_once "Router.php";
+
+function startRouter(): Router 
+{
+    $routes = [];
+
+    include_once "Routes/DomainRoutes.php";
+    $routes = array_merge($routes, DomainRoutes::getRoutes());
+
+    $routesClass = [];
+    foreach ($routes as $route) {
+        $routesClass[] = Route::fromArray($route);
+    }
+
+    return new Router($routesClass);
+}
---
+++ src/Model/Domain/DomainModel.php
@@ -0,0 +1,67 @@
+<?php 
+
+namespace Src\Model\Domain;
+
+use Src\Model\DatabaseModel;
+use Src\Entity\Domain\Domain;
+
+final readonly class DomainModel extends DatabaseModel {
+
+    public function find(int $id): ?Domain
+    {
+        $query = <<<SELECT_QUERY
+                    SELECT
+                        D.id,
+                        D.name,
+                        D.code
+                    FROM
+                        domain D
+                    WHERE
+                        D.id = :id
+                SELECT_QUERY;
+
+        $parameters = [
+            'id' => $id
+        ];
+
+        $result = $this->primitiveQuery($query, $parameters);
+        
+        return $this->toDomain($result[0] ?? null);
+    }
+
+    /** @return Domain[] */
+    public function search(): array
+    {
+        $query = <<<SELECT_QUERY
+                    SELECT
+                        D.id,
+                        D.name,
+                        D.code
+                    FROM 
+                        domain D
+                SELECT_QUERY;
+
+        $primitiveResults = $this->primitiveQuery($query);
+
+        $objectResults = [];
+        
+        foreach ($primitiveResults as $primitiveResult) {
+            $objectResults[] = $this->toDomain($primitiveResult);
+        }
+
+        return $objectResults;
+    }
+
+    private function toDomain(?array $primitive): ?Domain
+    {
+        if ($primitive === null) {
+            return null;
+        }
+
+        return new Domain(
+            $primitive['id'],
+            $primitive['name'],
+            $primitive['code']
+        );
+    }
+}
